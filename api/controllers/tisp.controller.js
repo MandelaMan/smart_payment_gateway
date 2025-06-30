dcdc5e6ba960baf3220ca4d56bcbf414bd5a0e68
MandelaMan <nelsonomoro@gmail.com>
2025-06-30 16:19:07 +0300

    added frontend

--- api/controllers/tisp.controller.js
+++ api/controllers/tisp.controller.js
@@ -1,14 +1,22 @@
+const axios = require("axios");
+const moment = require("moment");
+require("dotenv").config();
+
+const getTISPCustomer = async () => {
+  const data = {
+    dueDate: "30/06/2023",
+    isActive: true,
+  };
+  return data;
+};
+
+const test = (req, res) => {
+  res.json({
+    message: "ok",
+  });
+};
+
 module.exports = {
-  getTISPCustomer: async (customerNo) => {
-    const data = {
-      dueDate: "30/06/2023",
-      isActive: true,
-    };
-    return data;
-  },
-  test: (req, res) => {
-    res.json({
-      message: "ok",
-    });
-  },
+  getTISPCustomer,
+  test,
 };
--- api/index.js
+++ api/index.js
@@ -1,36 +1,80 @@
+// const express = require("express");
+// const cookieParser = require("cookie-parser");
+// const path = require("path");
+// var cors = require("cors");
+// require("dotenv").config();
+
+// //import Routes
+// const ussdRoutes = require("./routes/ussd.route");
+// // const zohoRoutes = require("./routes/zoho.route");
+// const customerRoutes = require("./routes/customer.route");
+// const mpesaRoutes = require("./routes/mpesa.route");
+
+// const app = express();
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(cors());
+
+// app.use("/ussd", ussdRoutes);
+// // app.use("/zoho", zohoRoutes);
+// app.use("/customer", customerRoutes);
+// app.use("/mpesa", mpesaRoutes);
+
+// app.get("/", (req, res) => {
+//   res.json({ message: "Hello. Welcome to Starlynx API!" });
+// });
+
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, () => {
+//   console.log("Server is up and running " + PORT);
+// });
+
+// app.use((err, req, res, next) => {
+//   const statusCode = err.statusCode || 500;
+//   const message = err.message || "Internal server error";
+
+//   return res.status(statusCode).json({
+//     success: false,
+//     statusCode,
+//     message,
+//   });
+// });
+
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const path = require("path");
-var cors = require("cors");
+const cors = require("cors");
 require("dotenv").config();
 
-//import Routes
+// Import routes
 const ussdRoutes = require("./routes/ussd.route");
-// const zohoRoutes = require("./routes/zoho.route");
 const customerRoutes = require("./routes/customer.route");
 const mpesaRoutes = require("./routes/mpesa.route");
 
 const app = express();
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 
+// Serve static files from Vite React build
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+// API Routes
 app.use("/ussd", ussdRoutes);
-// app.use("/zoho", zohoRoutes);
 app.use("/customer", customerRoutes);
 app.use("/mpesa", mpesaRoutes);
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello. Welcome to Starlynx API!" });
+// Catch-all route to serve React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log("Server is up and running " + PORT);
-});
-
+// Error handling middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal server error";
@@ -40,4 +84,10 @@ app.use((err, req, res, next) => {
     statusCode,
     message,
   });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server is up and running on port " + PORT);
 });
