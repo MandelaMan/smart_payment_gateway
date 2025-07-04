d0a701a5540df5c7223a943652966eee372e3135
MandelaMan <nelsonomoro@gmail.com>
2025-07-04 11:18:56 +0300

    fixed tisp endpoint

--- api/controllers/customer.controller.js
+++ api/controllers/customer.controller.js
@@ -2,23 +2,35 @@ const moment = require("moment");
 const { getSpecificCustomer } = require("./zoho.controller");
 const { getTISPCustomer } = require("./tisp.controller");
 
-module.exports = {
-  reportTransactions: (req, res) => {
-    res.json({
-      message: "ok",
-    });
-  },
-  getCustomerDetails: async (customerNo) => {
-    const customerDetails = {
-      ...(await getSpecificCustomer(customerNo)),
-      ...(await getTISPCustomer(client)),
-    };
+const reportTransactions = () => {
+  res.json({
+    message: "ok",
+  });
+};
 
-    return customerDetails;
-  },
-  test: (req, res) => {
-    res.json({
-      message: "ok",
-    });
-  },
+const getCustomerDetails = async (client) => {
+  const customerDetails = {
+    ...(await getSpecificCustomer(client)),
+    ...(await getTISPCustomer(client)),
+  };
+
+  return customerDetails;
+};
+
+const test = async (req, res) => {
+  const { client } = req.body;
+
+  const result = await getCustomerDetails(client);
+
+  // res.json({
+  //   message: "ok",
+  // });
+
+  res.json(result);
+};
+
+module.exports = {
+  reportTransactions,
+  getCustomerDetails,
+  test,
 };
--- api/routes/customer.route.js
+++ api/routes/customer.route.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   getCustomerDetails,
   reportTransactions,
+  test,
 } = require("../controllers/customer.controller");
 
 const router = express.Router();
 
 router.post("/", reportTransactions);
 router.get("/:customerNo", getCustomerDetails);
+router.post("/test", test);
 
 module.exports = router;
